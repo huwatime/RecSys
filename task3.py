578ea2738eff6880022ae2e17517b44fbcaa2742
yl100 <circle40191@gmail.com>
2018-06-25 14:33:19 +0200

    Add task3 draft2
     add biased_ALS with implicit feedback, still buggy

--- task3.py
+++ task3.py
@@ -126,7 +126,6 @@ class RecSys:
     def biased_ALS(self, lamda=0.01):
         '''
         lamda: regularization parameter
-        alpha: learning rate
         >>> recsys = RecSys()
         >>> recsys.load_ratings("testcase_ratings.csv")
         #>>> recsys.load_ratings("../Dataset/ratings_Electronics_50.csv")
@@ -143,23 +142,57 @@ class RecSys:
         betas = np.zeros((n, 1)) # users' biases
         gammas = np.zeros((m, 1)) # items' biases
 
-        aug_U = np.concatenate((np.atleast_2d(np.ones(n)).T, U), axis=1)
-        aug_V = np.dot(
-                    np.linalg.inv(np.dot(aug_U.T, aug_U) + lamda * np.identity(rank + 1)),
-                    csr_matrix(aug_U.T).dot(self.util_mat - betas.repeat(m, axis=1))).T
-        gammas = aug_V[:,0]
-        V = aug_V[:,1:]
+        # TODO comment
+        alpha = self.util_mat.count_nonzero() / n / m
 
-        print(aug_V.shape)
-        print(gammas.shape)
-        print(V.shape)
-        aug_V = np.concatenate((np.atleast_2d(np.ones(n)).T, V), axis=1)
-        aug_U = np.dot(
-                   np.linalg.inv(np.dot(aug_V.T, aug_V) + lamda * np.identity(rank + 1)),
-                   csr_matrix(aug_V.T).dot(self.util_mat - gammas.repeat(n, axis=1).T)).T
-        betas = aug_U[:,0]
-        U = aug_U[:,1:]
+        for rnd in range(20):
+            aug_U = np.concatenate((np.atleast_2d(np.ones(n)).T, U), axis=1)
+            for j in range(m):
+                r_j = np.where(self.util_mat[:,j].todense() > 0, 1, 0)
+                # FIXME need to understand the last dot term
+                aug_V_j = np.dot(
+                            np.linalg.inv(
+                                np.dot(aug_U.T, aug_U) +
+                                lamda * np.identity(rank + 1) +
+                                alpha * np.dot((r_j * aug_U).T, aug_U)),
+                            np.dot(
+                                aug_U.T,
+                                np.array(self.util_mat[:,j] - betas) * np.array(alpha * r_j + 1)))
+                gammas[j] = aug_V_j[0]
+                V[j,:] = aug_V_j[1:].T
 
-        print(aug_U.shape)
-        print(betas.shape)
-        print(U.shape)
+            aug_V = np.concatenate((np.atleast_2d(np.ones(m)).T, V), axis=1)
+            for i in range(n):
+                r_i = np.where(self.util_mat[i].T.todense() > 0, 1, 0)
+                # FIXME need to understand the last dot term
+                aug_U_i = np.dot(
+                            np.linalg.inv(
+                                np.dot(aug_V.T, aug_V) +
+                                lamda * np.identity(rank + 1) +
+                                alpha * np.dot((r_i * aug_V).T, aug_V)),
+                            np.dot(
+                                aug_V.T,
+                                np.array(self.util_mat[i].T - gammas) * np.array(alpha * r_i + 1)))
+                betas[i] = aug_U_i[0]
+                U[i,:] = aug_U_i[1:].T
+
+#        for rnd in range(10):
+#            # TODO comment
+#            aug_U = np.concatenate((np.atleast_2d(np.ones(n)).T, U), axis=1)
+#            aug_V = np.dot(
+#                        np.linalg.inv(np.dot(aug_U.T, aug_U) + lamda * np.identity(rank + 1)),
+#                        csr_matrix(aug_U.T).dot(self.util_mat - betas.repeat(m, axis=1))).T
+#            gammas = aug_V[:,0]
+#            V = aug_V[:,1:]
+#
+#            aug_V = np.concatenate((np.atleast_2d(np.ones(m)).T, V), axis=1)
+#            aug_U = np.dot(
+#                       np.linalg.inv(np.dot(aug_V.T, aug_V) + lamda * np.identity(rank + 1)),
+#                       csr_matrix(aug_V.T).dot(self.util_mat.T - gammas.repeat(n, axis=1))).T
+#            betas = aug_U[:,0]
+#            U = aug_U[:,1:]
+#
+        print(self.util_mat.todense())
+        print(np.dot(U, V.T) + betas.repeat(m, axis=1) + gammas.T.repeat(n, axis=0))
+        print(betas)
+        print(gammas)
