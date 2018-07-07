24418582d14234df4a0911eb4dc4777edc30544c
yl100 <circle40191@gmail.com>
2018-07-07 15:54:05 +0200

    Fix style error

--- main.py
+++ main.py
@@ -6,15 +6,17 @@ from EvaSys import EvaSys
 parser = argparse.ArgumentParser(
         description='Evaluate the recommender system with the ratings given.')
 parser.add_argument('model', metavar='model', type=str, nargs=1,
-        choices=['BLRS','ARS'],
-        help='The recommender system to evaluate ("BLRS"|"ARS")')
+                    choices=['BLRS', 'ARS'],
+                    help='The recommender system to evaluate ("BLRS"|"ARS")')
 parser.add_argument('-f', dest='files', metavar='file', type=str, nargs='+',
-        help='The path(s) of the rating file(s). If multiple files are \
-                specifiled, they will be treated as split files and be \
-                applied directly to k-fold evaluation manner, where k \
-                euqals the number of files you specifiled.')
+                    help='The path(s) of the rating file(s). If multiple \
+                            files are specifiled, they will be treated as \
+                            split files and be applied directly to k-fold \
+                            evaluation manner, where k euqals the number of \
+                            files you specifiled.')
 parser.add_argument('-k', dest='ks', metavar='K', type=int, nargs='+',
-        help='The value of K(s) of the evaluation matrics P@K, R@K, etc')
+                    help='The value of K(s) of the evaluation matrics P@K, \
+                            R@K, etc')
 
 args = parser.parse_args()
 model = args.model[0]
